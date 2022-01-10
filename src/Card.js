c367cdb88d64af3c48a499e63a480048ea3058d9
docteurjs <mahmoudbarrysn@gmail.com>
2022-01-10 16:52:59 +0000

    creating Card component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+    return(
+        <div>
+            <img alt="photo" src='https://robohash.org/robo1?200x200' />
+            <div>
+                <h1>jane Doe</h1>
+                <p>jane.does@gmail.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
