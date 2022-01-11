cacf43534a7eef455a71c871114359e081882059
docteurjs <mahmoudbarrysn@gmail.com>
2022-01-11 16:24:18 +0000

    search is ok

--- src/App.js
+++ src/App.js
@@ -17,19 +17,19 @@ class App extends Component{
         this.setState({
             searchfield: event.target.value
         })
+    }
+
+    render(){
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase()
             )}
         )
-        console.log(filteredRobots);
-    }
 
-    render(){
         return(
             <div className="tc">
                 <h1>Robofriends</h1>
                 <SearchBox searchChange = {this.onSearchChange}/>
-                <CardList robots={this.state.robots } />
+                <CardList robots={filteredRobots } />
             </div>
             
         );
